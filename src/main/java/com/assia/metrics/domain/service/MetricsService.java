bf2f80c1a06821fcf368abd62e265eb989beed77
beyzasenoll <beyzasenol08@gmail.com>
2023-08-01 11:37:58 +0300

    remove generated .class files after compilation

--- src/main/java/com/assia/metrics/domain/service/MetricsService.java
+++ src/main/java/com/assia/metrics/domain/service/MetricsService.java
@@ -168,7 +168,7 @@ public class MetricsService {
     public void deleteAlgorithmClassFile() {
         final Logger logger = LoggerFactory.getLogger(MetricsService.class);
         String classFilePath = externalClassFilePath + externalClassFileName + ".class";
-        File classFile = new File(classFilePath);
+        File classFile = new File(clasilePath);
 
         if (classFile.exists()) {
             if (classFile.delete()) {
